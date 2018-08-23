fb560b9f56c692aa4437e33373ba22f49af85a4a
deevarvar <zhih>
2018-08-23 23:24:50 +0800

    add initial version and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Examples in Fluent Python
+Just write some examples in **Fluent Python**
+
+## Examples list
+* flags in Chapter 17 Concurrency with futures
---
+++ flags.py
@@ -0,0 +1,11 @@
+# -*- coding=utf-8 -*-
+# author: zhihuaye@gmail.com
+
+"""
+1. try to get flags
+https://www.countries-ofthe-world.com/flags-of-the-world.html
+2. find the sample url
+https://www.countries-ofthe-world.com/flags-normal/flag-of-China.png
+3. add UA to get file
+"""
+
